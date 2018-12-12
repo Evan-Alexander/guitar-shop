297255add405d0acbd725181fa71e1b5074a3745
jb <jasontbrown99@gmail.com>
2018-12-12 15:48:36 -0800

    hash new passwords; create user login endpoint

--- server/server.js
+++ server/server.js
@@ -29,6 +29,28 @@ app.post('/api/users/register', (req, res) => {
       userdata: doc
     })
   })
+});
+
+app.post('/api/users/login',(req, res) => {
+  // find the email
+  User.findOne({'email':req.body.email}, (err, user) => {
+    if(!user) return res.json({ loginSuccess: false, message: 'Auth failed, email not found.'});
+
+     // check the password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({loginSuccess: false, message: 'Wrong password'});
+
+      // generate a token
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie('w_auth', user.token).status(200).json({
+          loginSuccess: true
+        })
+      })
+    })
+  })
+ 
+
 })
 
 
