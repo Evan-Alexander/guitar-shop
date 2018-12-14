6890f6f17d6ff694da9d2b65abfdccf7b23b76f4
jb <jasontbrown99@gmail.com>
2018-12-14 13:54:45 -0800

    add product model; post and getByDate and getById endpoints

--- server/server.js
+++ server/server.js
@@ -17,10 +17,63 @@ app.use(cookieParser());
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
 const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 // Middleware
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
+
+//=====================
+//  PRODUCTS / GUITARS
+//=====================
+
+// GET BY ARRIVAL
+
+// GET BY AMOUNT SOLD
+app.get('/api/product/articles', (req, res) => {
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+  Product.find().populate('brand').populate('wood')
+    .sort([[sortBy, order]]).limit(limit)
+    .exec((err, articles) => {
+      if(err) return res.status(400).send(err);
+      res.send(articles)
+    })
+})
+
+// GET BY ID
+app.get('/api/product/articles_by_id', (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if(type === 'array') {
+    let ids = req.body.id.split(',');
+    items = [];
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item)
+    })
+  }
+  Product.find({ $in: items })
+    populate('brand').populate('wood').exec((err, docs) => {
+    return res.status(200).send(docs)
+  })
+});
+
+// ADD NEW PRODUCTS
+app.post('/api/product/article',auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if(err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc
+    })
+  })
+})
+
 //===================
 //  WOOD TYPES
 //===================
