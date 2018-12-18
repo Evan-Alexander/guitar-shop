0a86acdc823f8ae85f65f64a3bfa765e383dc7d8
jb <jasontbrown99@gmail.com>
2018-12-18 14:20:17 -0800

    add protected routing; initial dashboard responsiveness

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import UserLayout from '../../hoc/user_layout';
+import CustomButton from '../utils/button';
 
 function UserDashboard() {
   return (
-    <div>
-      dashboard
-    </div>
+    <UserLayout>
+      <div className="user_nfo_panel">
+        <h1>User Information</h1>
+        <div>
+          <span>name</span>
+          <span>lastname</span>
+          <span>email</span>
+        </div>
+        <CustomButton 
+          type="default"
+          title="Edit account info"
+          linkTo="/user/user_profile"
+        />
+      </div>
+      <div className="user_nfo_panel">
+        <h1>Purchase history</h1>
+        <div className="user_product_block_wrapper">
+          history
+        </div>
+
+      </div>
+    </UserLayout>
   )
 }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import './styles.scss';
 
 import Layout from './hoc/layout';
+import Auth from './hoc/Auth';
+
 import Home from './components/Home';
 import RegisterLogin from './components/Register_login';
 import Register from './components/Register_login/Register';
@@ -13,10 +15,11 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/user/dashboard" exact component={UserDashboard} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/register_login" exact component={RegisterLogin} />
-        <Route path="/" exact component={Home} />
+        <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
+        
+        <Route path="/register" exact component={Auth(Register, false)} />
+        <Route path="/register_login" exact component={Auth(RegisterLogin, false)} />
+        <Route path="/" exact component={Auth(Home, null)} />
       </Switch>
     </Layout>
 
