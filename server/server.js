79ae7dfc3e2f745e603cacab28fb660dbb510e40
jb <jasontbrown99@gmail.com>
2019-01-24 15:38:47 -0800

    and again

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(express.static('client/build'))
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 cloudinary.config({
