e142967ce89992cd92c1077d960a6162c0de5623
jb <jasontbrown99@gmail.com>
2019-01-24 13:57:59 -0800

    and now?

--- server/server.js
+++ server/server.js
@@ -454,14 +454,13 @@ app.post('/api/site/site_data', auth, admin, (req, res) => {
 })
 
 // Default Production URL
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-if( process.env.NODE_ENV === 'production' ) {
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
 }
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 const port = process.env.PORT || 3002;
